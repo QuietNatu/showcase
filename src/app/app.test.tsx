172b6c91fd2eb484dd70e2b34bd5f94bff88c554
QuietNatu <quietnatu@outlook.com>
2022-12-31 20:49:07 +0000

    Setup accessibility testing

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import { axe } from '../test/utils/axe';
 import { render } from '../test/utils/render';
 import App from './app';
 
@@ -6,4 +7,9 @@ test('renders', () => {
   render(<App />);
 
   expect(screen.getByText('Vite + React')).toBeInTheDocument();
+});
+
+test('has no accessibility violations', async () => {
+  const { baseElement } = render(<App />);
+  expect(await axe(baseElement)).toHaveNoViolations();
 });
--- src/test/extend-matchers.ts
+++ src/test/extend-matchers.ts
@@ -1,5 +1,6 @@
 // TODO: delete this once https://github.com/testing-library/jest-dom/issues/427 is solved
 import matchers, { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+import { toHaveNoViolations } from 'jest-axe';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -7,8 +8,11 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     interface JestAssertion<T = any>
       extends jest.Matchers<void, T>,
-        TestingLibraryMatchers<T, void> {}
+        TestingLibraryMatchers<T, void> {
+      toHaveNoViolations(): T; // TODO: delete this once jest-axe stops referencing jest or when a vitest-axe alternative gets popular
+    }
   }
 }
 
 expect.extend(matchers);
+expect.extend(toHaveNoViolations);

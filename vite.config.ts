c178ddb99d1088f60980fa886f93e6d474e7a3e7
QuietNatu <quietnatu@outlook.com>
2022-12-31 21:23:54 +0000

    Configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,47 @@ import react from '@vitejs/plugin-react';
 import { vitePlugins } from './vite-plugins';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ...vitePlugins],
-
-  // https://github.com/vitest-dev/vitest
-  test: {
-    globals: true,
-    css: false,
-    include: ['src/**/*.test.*'],
-    environment: 'jsdom',
-    setupFiles: 'src/test/setup-tests.ts',
-    coverage: {
-      // threshold
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-
-      // config
-      provider: 'istanbul',
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: [
-        '**/*.test.*',
-        '**/*.stories.*',
-        'src/test',
-        'src/mocks',
-        'src/stories',
-        'src/main.tsx',
-        'src/vite-env.d.ts',
-        'src/service-worker.ts',
-      ],
-      reporter: ['lcov', 'text-summary'],
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      open: mode !== 'test',
     },
-  },
+
+    build: {
+      outDir: './build',
+    },
+
+    plugins: [react(), ...vitePlugins],
+
+    // https://github.com/vitest-dev/vitest
+    test: {
+      globals: true,
+      css: false,
+      include: ['src/**/*.test.*'],
+      environment: 'jsdom',
+      setupFiles: 'src/test/setup-tests.ts',
+      coverage: {
+        // threshold
+        branches: 90,
+        functions: 90,
+        lines: 90,
+        statements: 90,
+
+        // config
+        provider: 'istanbul',
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: [
+          '**/*.test.*',
+          '**/*.stories.*',
+          'src/test',
+          'src/mocks',
+          'src/stories',
+          'src/main.tsx',
+          'src/vite-env.d.ts',
+          'src/service-worker.ts',
+        ],
+        reporter: ['lcov', 'text-summary'],
+      },
+    },
+  };
 });

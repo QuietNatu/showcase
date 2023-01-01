cbd35eb4834169d658c49aa3a1c949ce5977d2d2
QuietNatu <quietnatu@outlook.com>
2023-01-01 16:06:25 +0000

    Setup Progressive Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vitePlugins } from './vite-plugins';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const baseUrl = env.VITE_BASE_URL;
+  const isE2e = env.VITE_E2E === 'true';
+
   return {
     server: {
       open: mode !== 'test',
@@ -15,7 +20,12 @@ export default defineConfig(({ mode }) => {
       outDir: './build',
     },
 
-    plugins: [react(), visualizer({ filename: 'dependency-stats.html' }), ...vitePlugins],
+    plugins: [
+      react(),
+      VitePWA(isE2e ? { injectRegister: null } : pwaOptions),
+      visualizer({ filename: 'dependency-stats.html' }),
+      ...vitePlugins,
+    ],
 
     // https://github.com/vitest-dev/vitest
     test: {
@@ -49,3 +59,36 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  strategies: 'injectManifest',
+  srcDir: 'src',
+  filename: 'service-worker.ts',
+  includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'Showcase',
+    short_name: 'Showcase',
+    description: 'A project to practice and showcase what I have learned',
+    theme_color: '#222222',
+    background_color: '#222222',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+};

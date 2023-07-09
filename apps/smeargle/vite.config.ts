135c4eeecb642d5e6bd7d942242dede578b78874
QuietNatu <quietnatu@outlook.com>
2023-07-09 16:30:09 +0100

    Setup react app

---
+++ apps/smeargle/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+
+    server: {
+      open: mode !== 'test',
+    },
+
+    build: {
+      outDir: './build',
+    },
+  };
+});

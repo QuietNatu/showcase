7bbca0ed258e29fcd21102cc40e18d23dfd06e08
QuietNatu <quietnatu@outlook.com>
2023-07-17 23:48:27 +0100

    feat(smeargle): add msw to app

--- apps/smeargle/src/main.tsx
+++ apps/smeargle/src/main.tsx
@@ -3,6 +3,12 @@ import { App } from './app.tsx';
 import './index.css';
 import { StrictMode } from 'react';
 
+if (import.meta.env.DEV) {
+  const { mockWorker } = await import('@/mocks/server/browser');
+
+  await mockWorker.start({ onUnhandledRequest: 'bypass' });
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

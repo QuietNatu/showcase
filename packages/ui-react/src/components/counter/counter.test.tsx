9944f3850f4240fc45c1f80ce726b3c7a4af9f34
QuietNatu <quietnatu@outlook.com>
2023-07-20 21:57:20 +0100

    refactor: force relative imports for packages

--- packages/ui-react/src/components/counter/counter.test.tsx
+++ packages/ui-react/src/components/counter/counter.test.tsx
@@ -1,8 +1,6 @@
 import { screen } from '@testing-library/react';
-import { render } from '../../test/utils/render';
 import { Counter } from './counter';
-
-/* TODO: absolute imports? */
+import { render } from '../../test';
 
 test('renders', async () => {
   const { userEvent } = render(<Counter />);

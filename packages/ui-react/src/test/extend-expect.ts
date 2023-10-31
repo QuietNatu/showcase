f5ee83e77821c6f2c7c47893612797ce065e1451
QuietNatu <quietnatu@outlook.com>
2023-10-31 21:15:24 +0000

    refactor: replace vitest-dom with jest-dom
    vitest-dom is deprecated since jest-dom supports vitest

--- packages/ui-react/src/test/extend-expect.ts
+++ packages/ui-react/src/test/extend-expect.ts
@@ -1,16 +1,10 @@
-import { TestingLibraryMatchers } from 'vitest-dom/dist/matchers';
-import * as domMatchers from 'vitest-dom/matchers';
+import '@testing-library/jest-dom/vitest';
 import * as axeMatchers from 'vitest-axe/matchers';
+import type { AxeMatchers } from 'vitest-axe';
 
 declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends TestingLibraryMatchers<typeof expect.stringContaining, T> {
-    tohaveNoViolations: () => void;
-  }
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<unknown, unknown> {
-    tohaveNoViolations: () => void;
-  }
+  export interface Assertion extends AxeMatchers {}
+  export interface AsymmetricMatchersContaining extends AxeMatchers {}
 }
 
-expect.extend(domMatchers);
 expect.extend(axeMatchers);

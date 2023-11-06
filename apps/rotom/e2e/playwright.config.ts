7dfd8c2bc0b6129ceb43fdca9efa0fa55f388628
QuietNatu <quietnatu@outlook.com>
2023-11-06 19:30:41 +0000

    feat: add e2e to rotom

---
+++ apps/rotom/e2e/playwright.config.ts
@@ -0,0 +1,43 @@
+import { devices, type PlaywrightTestConfig } from '@playwright/test';
+
+const port = process.env['PORT'] ? Number(process.env['PORT']) : 6104;
+const isCi = process.env['CI'] && process.env['CI'] !== '0';
+
+export default {
+  testMatch: /.*\.test\.ts/,
+  testDir: './tests',
+  outputDir: 'results',
+  forbidOnly: !!isCi,
+  retries: isCi ? 2 : 0,
+  maxFailures: 0,
+  reporter: [['list'], ['html', { outputFolder: 'report' }]],
+  use: {
+    baseURL: `http://localhost:${port}/`,
+    headless: true,
+    locale: 'en-GB',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: isCi
+      ? `pnpm run preview --port ${port}`
+      : `pnpm run dev --configuration production --open false --port ${port}`,
+    port,
+    reuseExistingServer: !isCi,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+} satisfies PlaywrightTestConfig;

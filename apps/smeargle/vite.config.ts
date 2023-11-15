893ef253e7a33940be22171d402f6af7c205825c
QuietNatu <quietnatu@outlook.com>
2023-11-15 00:42:01 +0000

    feat: add service worker to smeargle

--- apps/smeargle/vite.config.ts
+++ apps/smeargle/vite.config.ts
@@ -1,13 +1,17 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const isE2e = env.VITE_E2E === 'true';
+
   return {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), VitePWA(isE2e ? { injectRegister: null } : pwaOptions)],
 
     server: {
       open: mode !== 'test',
@@ -47,3 +51,65 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+
+/* TODO: file cache */
+const pwaOptions: Partial<VitePWAOptions> = {
+  includeAssets: ['favicon.ico'],
+  manifest: {
+    name: 'Rotom',
+    short_name: 'Rotom',
+    theme_color: '#1976d2',
+    background_color: '#fafafa',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'icons/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+      {
+        src: 'icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable any',
+      },
+    ],
+  },
+};

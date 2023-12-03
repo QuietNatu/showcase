33cc8a2c93692a88a8a02dbac72dacdfa42fe73c
QuietNatu <quietnatu@outlook.com>
2023-12-03 12:25:25 +0000

    feat: implement button variants

---
+++ packages/ui-angular/src/lib/components/button/button.directive.ts
@@ -0,0 +1,82 @@
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  Output,
+  Signal,
+  booleanAttribute,
+  computed,
+  signal,
+} from '@angular/core';
+import { VariantProps, cva } from 'class-variance-authority';
+
+export const buttonVariants = cva('natu-button', {
+  variants: {
+    variant: {
+      primary: 'natu-button--primary',
+      secondary: 'natu-button--secondary',
+    },
+    size: {
+      small: 'natu-button--small',
+      medium: 'natu-button--medium',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'medium',
+  },
+});
+
+export type NatuButtonVariants = VariantProps<typeof buttonVariants>;
+
+@Directive({
+  selector: '[natuButton]',
+  standalone: true,
+  host: {
+    '[class]': 'class$()',
+  },
+})
+export class NatuButtonDirective {
+  @Input() set variant(variant: NatuButtonVariants['variant']) {
+    this.variant$.set(variant);
+  }
+
+  @Input() set size(size: NatuButtonVariants['size']) {
+    this.size$.set(size);
+  }
+
+  readonly class$: Signal<string>;
+
+  private readonly variant$ = signal<NatuButtonVariants['variant']>('primary');
+  private readonly size$ = signal<NatuButtonVariants['size']>('medium');
+
+  constructor() {
+    this.class$ = computed(() => buttonVariants({ variant: this.variant$(), size: this.size$() }));
+  }
+}
+
+@Directive({
+  selector: '[natuButton]:not(button)',
+  standalone: true,
+  host: {
+    role: 'button',
+    '[class.natu-button--active]': 'isActive$()',
+    '[class.natu-button--disabled]': 'disabled',
+    '[attr.tabindex]': '!disabled ? 0 : null',
+     
+    '(click)': '!disabled && handleClick() && natuButtonClick.emit($event)',
+    '(keydown.enter)': '!disabled && natuButtonClick.emit($event)',
+    '(keydown.space)': '!disabled && natuButtonClick.emit($event)',
+    '(mousedown)': '!disabled && isActive$.set(true)',
+    '(mouseup)': '!disabled && isActive$.set(false)',
+  },
+})
+export class NatuA11yButtonDirective {
+  @Input({ transform: booleanAttribute }) disabled = false;
+
+  @Output() natuButtonClick = new EventEmitter<Event>();
+
+  readonly isActive$ = signal(false);
+}
+
+export const natuButtonImports = [NatuButtonDirective, NatuA11yButtonDirective];
---
+++ packages/ui-angular/src/lib/components/button/button.stories.ts
@@ -0,0 +1,59 @@
+import { moduleMetadata, type Meta, type StoryObj, argsToTemplate } from '@storybook/angular';
+import { NatuButtonDirective, NatuButtonVariants, natuButtonImports } from './button.directive';
+
+interface StoryArgs extends NatuButtonDirective {
+  disabled: boolean;
+  variants: Array<NatuButtonVariants['variant']>;
+}
+
+const meta = {
+  title: 'Components/Button',
+  component: NatuButtonDirective,
+  tags: ['autodocs'],
+  decorators: [moduleMetadata({ imports: [natuButtonImports] })],
+  args: {
+    variants: ['primary', 'secondary'],
+  },
+  render: (args) => {
+    const templateVariants = args.variants.map((variant) => {
+      const buttonArgs = argsToTemplate(args, { exclude: ['variant', 'variants'] });
+      return `<button type="button" [natuButton] [variant]="'${variant}'" ${buttonArgs}>Button</button>`;
+    });
+
+    return {
+      props: args,
+      template: templateVariants.join(''),
+    };
+  },
+} satisfies Meta<StoryArgs>;
+
+export default meta;
+type Story = StoryObj<StoryArgs>;
+
+export const Default: Story = {};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const CustomElement: Story = {
+  render: (args) => {
+    const templateVariants = args.variants.map((variant) => {
+      const buttonArgs = argsToTemplate(args, { exclude: ['variant', 'variants'] });
+      return `<span [natuButton] [variant]="'${variant}'" ${buttonArgs}>Button</span>`;
+    });
+
+    return {
+      props: args,
+      template: templateVariants.join(''),
+    };
+  },
+};

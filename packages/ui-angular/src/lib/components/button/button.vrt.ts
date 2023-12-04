9e18dbf90ebb14f104a144f69c537670eba3b890
QuietNatu <quietnatu@outlook.com>
2023-12-04 00:45:16 +0000

    test: add vrt scenarios

---
+++ packages/ui-angular/src/lib/components/button/button.vrt.ts
@@ -0,0 +1,21 @@
+import { VrtScenario, createVrtStorybookScenarios } from '@natu/vrt';
+import { test } from '@playwright/test';
+
+const scenarios: VrtScenario[] = [
+  { story: 'default' },
+  { story: 'small' },
+  { story: 'disabled' },
+  { story: 'custom-element' },
+];
+
+const testScenarios = createVrtStorybookScenarios({
+  scenarios,
+  page: 'components-button',
+  viewports: [{ name: 'custom', width: 300, height: 100 }], // TODO: adjust this
+});
+
+testScenarios.forEach((scenario) => {
+  test(scenario.id, async ({ page }, testInfo) => {
+    await scenario.test(page, testInfo);
+  });
+});

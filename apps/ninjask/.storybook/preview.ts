4b063ad24ce29b7c2724fc6d1ed441f65916e913
QuietNatu <quietnatu@outlook.com>
2023-12-05 23:26:29 +0000

    feat: add storybook theme buttons

--- apps/ninjask/.storybook/preview.ts
+++ apps/ninjask/.storybook/preview.ts
@@ -11,6 +11,33 @@ const preview: Preview = {
       },
     },
   },
+  globalTypes: {
+    theme: {
+      description: 'The visual theme of the story',
+      defaultValue: 'ninjask',
+      toolbar: {
+        dynamicTitle: true,
+        icon: 'paintbrush',
+        items: [
+          { value: 'ninjask', title: 'Ninjask' },
+          { value: 'rotom', title: 'Rotom' },
+          { value: 'smeargle', title: 'Smeargle' },
+        ],
+      },
+    },
+    colorScheme: {
+      description: 'The color scheme of the story',
+      defaultValue: 'light',
+      toolbar: {
+        dynamicTitle: true,
+        icon: 'starhollow',
+        items: [
+          { value: 'light', title: 'Light' },
+          { value: 'Dark', title: 'Dark' },
+        ],
+      },
+    },
+  },
 };
 
 export default preview;

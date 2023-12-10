8bd9e9bcfaabb097bcd00581b555c59ab1d809da
QuietNatu <quietnatu@outlook.com>
2023-12-10 13:14:18 +0000

    chore: configure vite and typescript

---
+++ packages/styles/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+/* TODO: turbo outputs for this particular build */
+
+// https://vitejs.dev/config/
+export default defineConfig(({}) => {
+  return {
+    plugins: [],
+  };
+});

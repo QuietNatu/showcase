c3643aac2aeb665b7598a40ecaef61ddf7e53b24
QuietNatu <quietnatu@outlook.com>
2023-12-11 23:35:39 +0000

    feat: support themes, replace scss themes

---
+++ packages/styles/tokens/config.js
@@ -0,0 +1,63 @@
+import StyleDictionary from 'style-dictionary';
+
+const { fileHeader, formattedVariables } = StyleDictionary.formatHelpers;
+const themes = ['ninjask', 'rotom', 'smeargle'];
+
+StyleDictionary.registerFormat({
+  name: 'css/variables-themed',
+  formatter: ({ dictionary, file, options }) => {
+    const { outputReferences, selector } = options;
+
+    const header = fileHeader({ file });
+    const variables = formattedVariables({
+      format: 'css',
+      dictionary,
+      outputReferences,
+    });
+
+    return `${header}${selector} {\n${variables}\n}\n`;
+  },
+});
+
+for (const theme of themes) {
+  const config = getStyleDictionaryConfig(theme);
+  const sd = new StyleDictionary(config);
+  await sd.buildAllPlatforms();
+}
+
+function getStyleDictionaryConfig(theme) {
+  const themeSelector = `:root[data-theme='${theme}']`;
+
+  return {
+    /* TODO: dark mode and include defaults for dark mode */
+    source: [`tokens/themes/${theme}/*.json5`, 'tokens/default/**/*.json5'],
+    platforms: {
+      scss: {
+        transforms: ['attribute/cti', 'name/cti/kebab'],
+        buildPath: `scss/tokens/${theme}/`,
+        prefix: 'natu',
+        files: [
+          {
+            destination: '_light.scss',
+            format: 'css/variables-themed',
+            options: {
+              outputReferences: true,
+              theme,
+              selector: themeSelector,
+            },
+          },
+          {
+            destination: '_dark.scss',
+            format: 'css/variables-themed',
+            filter: (token) => token.attributes.theme === 'dark',
+            options: {
+              outputReferences: true,
+              theme,
+              selector: `${themeSelector}[data-color-scheme='dark']`,
+            },
+          },
+        ],
+      },
+    },
+  };
+}

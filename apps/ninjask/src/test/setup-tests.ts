3f2c5b0fbb52f4528cd8a20bd938342df597fa8f
QuietNatu <quietnatu@outlook.com>
2023-12-14 11:54:52 +0000

    fix: temporarily disable msw in solid tests

--- apps/ninjask/src/test/setup-tests.ts
+++ apps/ninjask/src/test/setup-tests.ts
@@ -1,12 +1,13 @@
 import '@natu/ui-solid/test/extend-expect';
 
 import { mockDatabase } from '@/mocks/server/database/database';
-import { mockServer } from '@/mocks/server/server';
+// TODO: commented while https://github.com/solidjs/vite-plugin-solid/issues/125 is not solved
+// import { mockServer } from '@/mocks/server/server';
 import { drop } from '@mswjs/data';
 import { deleteAllCookies } from '@natu/utils';
 
 beforeAll(() => {
-  mockServer.listen();
+  // mockServer.listen();
 });
 
 afterEach(() => {
@@ -15,10 +16,10 @@ afterEach(() => {
 
   deleteAllCookies();
 
-  mockServer.resetHandlers();
+  // mockServer.resetHandlers();
   drop(mockDatabase);
 });
 
 afterAll(() => {
-  mockServer.close();
+  // mockServer.close();
 });

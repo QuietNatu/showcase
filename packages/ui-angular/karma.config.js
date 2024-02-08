f47194c8fbe803d2ad9ec585703d77e260796601
QuietNatu <121642811+QuietNatu@users.noreply.github.com>
2024-02-08 01:21:29 +0000

    Remove Karma Jasmine HTML Reporter (#407)

--- packages/ui-angular/karma.config.js
+++ packages/ui-angular/karma.config.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
@@ -22,9 +21,6 @@ module.exports = function (config) {
         // or set a specific seed with `seed: 4321`
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
@@ -45,7 +41,7 @@ module.exports = function (config) {
         },
       },
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 5384,
     browsers: ['ChromeHeadless'],
     restartOnFileChange: true,

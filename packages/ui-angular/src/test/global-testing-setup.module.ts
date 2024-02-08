ccf51597636f0029d90d66db0b08e306fa24608f
QuietNatu <quietnatu@outlook.com>
2024-02-08 01:09:20 +0000

    test: setup ui config for tests

--- packages/ui-angular/src/test/global-testing-setup.module.ts
+++ packages/ui-angular/src/test/global-testing-setup.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NATU_UI_CONFIG, NatuUiConfig } from '../lib/core';
+
+const config: NatuUiConfig = {
+  tooltip: { hoverDelay: 0 },
+};
 
 @NgModule({
-  providers: [],
+  providers: [{ provide: NATU_UI_CONFIG, useValue: config }],
 })
 export class GlobaltTestingSetupModule {}

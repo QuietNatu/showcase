42dfeaf244768471d7a038d9a5eef630c8288f92
QuietNatu <quietnatu@outlook.com>
2024-02-13 22:08:16 +0000

    chore: support variants in VRT

--- packages/vrt/src/types.ts
+++ packages/vrt/src/types.ts
@@ -7,25 +7,27 @@ export interface VrtViewport {
 }
 
 export interface VrtScenarioOptions {
-  /**
-   * The precision threshold between the produced screenshot and the saved screenshot.
-   */
+  /** The precision threshold between the produced screenshot and the saved screenshot. */
   threshold?: number;
-
-  /**
-   * Perform interactions after the page is mounted.
-   */
+  /** Perform interactions after the page is mounted. */
   onMount?: (page: Page) => Promise<void>;
+  /**
+   * List of different variations of Storybook global inputs that change the rendering of the story (like language or theme).
+   *
+   * Each variant will result in a new screenshot.
+   */
+  variants?: Array<Record<string, unknown>>;
 }
 
 export interface VrtScenario extends VrtScenarioOptions {
-  /**
-   * the name of the story.
-   */
+  /** The name of the story. */
   story: string;
-  /**
-   * Name to distinguish tests done one the same story.
-   */
+  /** Name to distinguish tests done one the same story. */
   name?: string;
+  /**
+   * The viewport to use for the test.
+   *
+   * Each viewport will result in a new screenshot.
+   */
   viewports?: VrtViewport[];
 }

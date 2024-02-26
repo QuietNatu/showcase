72402d573ea4475c66817f588dc35d907f4adfc5
QuietNatu <quietnatu@outlook.com>
2024-02-26 17:51:12 +0000

    feat: hide label when sidebar collapsed

--- packages/ui-angular/src/lib/components/sidebar/components/sidebar-item.component.ts
+++ packages/ui-angular/src/lib/components/sidebar/components/sidebar-item.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NatuSidebarService } from '../sidebar.service';
 
 @Component({
   selector: 'natu-sidebar-item,[natu-sidebar-item]',
   template: `
     <ng-content select="[natuSidebarItemIcon]" />
 
-    @if (isExpanded) {
+    @if (isExpanded$()) {
       <ng-content select="[natuSidebarItemLabel]" />
     }
   `,
@@ -13,5 +14,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
 })
 export class NatuSidebarItemComponent {
-  readonly isExpanded = true;
+  readonly isExpanded$;
+
+  private readonly sidebarService = inject(NatuSidebarService);
+
+  constructor() {
+    this.isExpanded$ = this.sidebarService.isExpanded$;
+  }
 }

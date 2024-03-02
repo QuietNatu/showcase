7a5d8e9298e46e55376aa32a802b5fec49cdac0a
Rui Nogueira <121642811+QuietNatu@users.noreply.github.com>
2024-03-02 18:09:05 +0000

    Angular - Add focus ring directive (#454)

---
+++ packages/ui-angular/src/lib/directives/focus-ring/focus-ring.directive.ts
@@ -0,0 +1,35 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { Directive, ElementRef, Input, Signal, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+/**
+ * Manages host element's focus ring.
+ */
+@Directive({
+  selector: '[natuFocusRing]',
+  standalone: true,
+  host: {
+    '[class]': 'focusVisibleClass && isFocusVisible() ? focusVisibleClass : null',
+  },
+})
+export class NatuFocusRingDirective {
+  @Input({ alias: 'natuFocusRingFocusVisibleClass' }) focusVisibleClass?: string;
+
+  readonly isFocusVisible: Signal<boolean>;
+
+  private readonly elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
+  private readonly focusMonitor = inject(FocusMonitor); // Change this once CDK directives are standalone...
+
+  constructor() {
+    this.isFocusVisible = this.getIsFocusVisible();
+  }
+
+  private getIsFocusVisible() {
+    const isFocusVisible$ = this.focusMonitor
+      .monitor(this.elementRef.nativeElement)
+      .pipe(map((origin) => origin === 'keyboard'));
+
+    return toSignal(isFocusVisible$, { initialValue: false });
+  }
+}
--- packages/ui-angular/src/lib/directives/index.ts
+++ packages/ui-angular/src/lib/directives/index.ts
@@ -1,3 +1,4 @@
 export * from './accordion';
 export * from './button/button.directive';
+export * from './focus-ring/focus-ring.directive';
 export * from './tooltip/tooltip.directive';

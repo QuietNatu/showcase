0b249817ed140631944205f77cc87bc47c3b2a76
QuietNatu <quietnatu@outlook.com>
2024-04-01 15:30:48 +0100

    feat: make dismiss button absolutely positioned on the card
    needed for scenarios like popover focus trap to make the dismiss button the last tabbable element

--- packages/ui-angular/src/lib/components/card/card.component.ts
+++ packages/ui-angular/src/lib/components/card/card.component.ts
@@ -5,26 +5,44 @@ import {
   Input,
   Output,
   booleanAttribute,
-  inject,
 } from '@angular/core';
 import { NatuCardHeaderComponent } from './components/card-header.component';
 import { NatuCardBodyComponent } from './components/card-body.component';
 import { NatuCardFooterComponent } from './components/card-footer.component';
 import { NatuCardHeaderIconDirective } from './directives/card-header-icon.directive';
-import { NatuCardService } from './card.service';
-import { injectRegisterIcons } from '@natu/assets';
+import { SvgIconComponent, injectRegisterIcons } from '@natu/assets';
 import { xIcon } from '@natu/assets/svg/x';
-import { registerEffect } from '../../utils/rxjs';
+import { natuButtonImports } from '../../directives';
+import { NatuCardService } from './card.service';
+
+/* TODO: add tooltip overflow later */
+/* TODO: add i18n once implemented */
 
 @Component({
   selector: 'natu-card,[natu-card]',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [natuButtonImports, SvgIconComponent],
   providers: [NatuCardService],
   template: `
     <ng-content select="natu-card-header,[natu-card-header]" />
     <ng-content />
     <ng-content select="natu-card-footer,[natu-card-footer]" />
+
+    @if (isDismissable) {
+      <button
+        type="button"
+        natuButton
+        [isIconButton]="true"
+        [variant]="'ghost'"
+        [size]="'small'"
+        class="natu-card__dismiss"
+        (click)="dismiss.emit()"
+      >
+        <span class="natu-visually-hidden">Dismiss</span>
+        <svg-icon [key]="'x'" aria-hidden="true" />
+      </button>
+    }
   `,
   host: {
     class: 'natu-card',
@@ -39,18 +57,12 @@ export class NatuCardComponent {
   @Input({ transform: booleanAttribute }) isEmbedded = false;
 
   /** Whether to show the dismissable button or not. */
-  @Input({ transform: booleanAttribute }) set isDismissable(isDismissable: boolean) {
-    this.cardService.setIsDismissable(isDismissable);
-  }
+  @Input({ transform: booleanAttribute }) isDismissable: boolean = false;
 
   @Output() dismiss = new EventEmitter<void>();
 
-  private readonly cardService = inject(NatuCardService);
-
   constructor() {
     injectRegisterIcons([xIcon]);
-
-    registerEffect(this.cardService.dismiss$, () => this.dismiss.emit());
   }
 }
 

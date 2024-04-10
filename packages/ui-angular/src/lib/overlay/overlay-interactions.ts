410bb555d0dc6c28078881e4ea60f3ed8c3ce9f1
QuietNatu <quietnatu@outlook.com>
2024-04-10 17:24:32 +0100

    fix: non button elements with popover trigger click of focus trapped elements

--- packages/ui-angular/src/lib/overlay/overlay-interactions.ts
+++ packages/ui-angular/src/lib/overlay/overlay-interactions.ts
@@ -1,6 +1,16 @@
 import { assertInInjectionContext, inject } from '@angular/core';
 import { NatuOverlayService } from './overlay.service';
-import { EMPTY, filter, fromEvent, map, merge, switchMap, timer } from 'rxjs';
+import {
+  EMPTY,
+  asyncScheduler,
+  filter,
+  fromEvent,
+  map,
+  merge,
+  observeOn,
+  switchMap,
+  timer,
+} from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { DOCUMENT } from '@angular/common';
 import { NatuPortalService } from '../portal';
@@ -100,6 +110,12 @@ export function useOverlayClick() {
   const customPress$ = customButtonElement$.pipe(
     switchMap((element) => fromEvent<KeyboardEvent>(element, 'keydown')),
     filter((event) => event.key === 'Enter' || event.key === ' '),
+    /**
+     * Delays Keydown event.
+     * Keydown event is triggered before a click event.
+     * If an overlay with focus trap is opened via keydown, a click event will be triggered on the newly focused element.
+     */
+    observeOn(asyncScheduler),
   );
 
   const effect$ = merge(click$, customPress$);

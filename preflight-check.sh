a038ce84f3008bbdeb94a74db6881df4481cc4bc
QuietNatu <quietnatu@outlook.com>
2024-05-29 11:49:53 +0000

    chore: add vrt to preflight check

--- preflight-check.sh
+++ preflight-check.sh
@@ -3,11 +3,6 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-# if ! docker info > /dev/null 2>&1; then
-#   echo -e "\n${RED}this script uses docker, and it isn't running - please start docker and try again!"
-#   exit 1
-# fi
-
 set -e
 
 export CI=1
@@ -17,5 +12,6 @@ pnpm i --frozen-lockfile
 pnpm run lint
 pnpm run test
 pnpm run build
+pnpm run vrt
 
 echo -e "\n${GREEN}preflight-check success"

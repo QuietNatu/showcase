22f99a31b7312cc2387ec4e81fbbe7c1dcad7007
QuietNatu <quietnatu@outlook.com>
2024-06-04 14:31:26 +0000

    test: add header VRT

--- packages/ui-react/src/lib/components/header/header.tsx
+++ packages/ui-react/src/lib/components/header/header.tsx
@@ -1,7 +1,9 @@
 import clsx from 'clsx';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-export const NatuHeader = forwardRef<HTMLElement, ComponentPropsWithoutRef<'header'>>(
+export type NatuHeaderProps = ComponentPropsWithoutRef<'header'>;
+
+export const NatuHeader = forwardRef<HTMLElement, NatuHeaderProps>(
   function NatuHeader(props, forwardedRef) {
     return (
       <header {...props} ref={forwardedRef} className={clsx('natu-header', props.className)}>

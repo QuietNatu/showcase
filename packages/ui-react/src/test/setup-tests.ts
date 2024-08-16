3841db3492150377411f2d93342b3fc4045a1313
QuietNatu <quietnatu@outlook.com>
2024-08-16 15:56:48 +0000

    feat: add i18n to ui-react

--- packages/ui-react/src/test/setup-tests.ts
+++ packages/ui-react/src/test/setup-tests.ts
@@ -1,6 +1,12 @@
 import '../lib/test/extend-expect';
 
 import { deleteAllCookies } from '@natu/utils';
+import { setupI18n } from '../mocks/i18n';
+
+// eslint-disable-next-line vitest/no-hooks
+beforeAll(async () => {
+  await setupI18n();
+});
 
 afterEach(() => {
   localStorage.clear();

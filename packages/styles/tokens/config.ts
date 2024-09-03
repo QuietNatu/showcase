2cb709098e7b4e97f640b51041643ab38e93eac9
QuietNatu <quietnatu@outlook.com>
2024-09-03 23:30:14 +0000

    refactor: apply required v4 migrations

--- packages/styles/tokens/config.ts
+++ packages/styles/tokens/config.ts
@@ -1,100 +1,80 @@
-import StyleDictionary, { Config } from 'style-dictionary';
-import { fileHeader, formattedVariables } from 'style-dictionary/utils';
+import StyleDictionary, { Config, TransformedToken } from 'style-dictionary';
 
 const themes = ['ninjask', 'rotom', 'smeargle'];
 const colorSchemes = ['light', 'dark'];
 
-/* TODO: https://styledictionary.com/version-4/migration/ */
-/* TODO: https://github.com/amzn/style-dictionary/blob/main/examples/advanced/multi-brand-multi-platform/build.js */
-
-StyleDictionary.registerFormat({
-  name: 'css/variables-themed',
-  format: async ({ dictionary, file, options }) => {
-    const { outputReferences, selector } = options;
-
-    const header = await fileHeader({ file });
-    const variables = formattedVariables({
-      format: 'css',
-      dictionary,
-      outputReferences,
-    });
-
-    return `${header}${selector} {\n${variables}\n}\n`;
-  },
-});
+const colorSchemeExclusionRegex = `!(*.${colorSchemes.join('|*.')})`;
 
 StyleDictionary.registerTransform({
   name: 'time/miliseconds',
   type: 'value',
-  filter: (token) => token.attributes.category === 'time',
+  filter: (token) => token.type === 'time',
   transform: (token) => token.original.value.toString() + 'ms',
 });
 
 for (const theme of themes) {
-  const selector = `:root[data-theme='${theme}']`;
+  for (let index = 0; index < colorSchemes.length; index++) {
+    const colorScheme = colorSchemes[index]!;
+    const isMainColorScheme = index === 0;
 
-  const lightConfig: Config = {
-    source: [
-      `tokens/default/**/!(*.${colorSchemes.join(`|*.`)}).json5`,
-      `tokens/themes/${theme}/**/!(*.${colorSchemes.join(`|*.`)}).json5`,
-    ],
-    platforms: {
-      scss: {
-        transforms: ['name/kebab', 'time/miliseconds', 'size/pxToRem'],
-        buildPath: `scss/tokens/${theme}/`,
-        prefix: 'natu',
-        files: [
-          {
-            destination: `_light.scss`,
-            format: `css/variables-themed`,
-            options: {
-              outputReferences: true,
-              selector,
+    const selector = isMainColorScheme
+      ? `:root[data-theme='${theme}']`
+      : `:root[data-theme='${theme}'][data-color-scheme='${colorScheme}']`;
+
+    const filter = isMainColorScheme
+      ? undefined
+      : (token: TransformedToken) => token.filePath.endsWith(`.${colorScheme}.json5`);
+
+    const tokenConfig: Partial<Config> = isMainColorScheme
+      ? {
+          source: [
+            `tokens/default/**/${colorSchemeExclusionRegex}.json5`,
+            `tokens/themes/${theme}/**/${colorSchemeExclusionRegex}.json5`,
+          ],
+        }
+      : {
+          include: [
+            `tokens/default/**/${colorSchemeExclusionRegex}.json5`,
+            `tokens/themes/${theme}/**/${colorSchemeExclusionRegex}.json5`,
+          ],
+          source: [
+            `tokens/default/**/*.${colorScheme}.json5`,
+            `tokens/themes/${theme}/**/*.${colorScheme}.json5`,
+          ],
+        };
+
+    const config: Config = {
+      ...tokenConfig,
+      platforms: {
+        scss: {
+          transforms: ['name/kebab', 'time/miliseconds', 'size/pxToRem'],
+          buildPath: `scss/tokens/${theme}/`,
+          prefix: 'natu',
+
+          files: [
+            {
+              destination: `_${colorScheme}.scss`,
+              format: `css/variables`,
+              filter,
+              options: {
+                selector,
+                outputReferences: true,
+              },
             },
-          },
-        ],
+          ],
+        },
       },
-    },
-  };
+    };
 
-  const darkConfig: Config = {
-    include: [
-      `tokens/default/**/!(*.${colorSchemes.join(`|*.`)}).json5`,
-      `tokens/themes/${theme}/**/!(*.${colorSchemes.join(`|*.`)}).json5`,
-    ],
-    source: [`tokens/default/**/*.dark.json5`, `tokens/themes/${theme}/**/*.dark.json5`],
-    platforms: {
-      scss: {
-        transforms: ['name/kebab', 'time/miliseconds'],
-        buildPath: `scss/tokens/${theme}/`,
-        prefix: 'natu',
-        files: [
-          {
-            destination: `_dark.scss`,
-            format: `css/variables-themed`,
-            filter: (token) => token.filePath.indexOf(`.dark`) !== -1,
-            options: {
-              outputReferences: true,
-              selector: `${selector}[data-color-scheme='dark']`,
-            },
-          },
-        ],
-      },
-    },
-  };
-
-  const lightSd = new StyleDictionary(lightConfig);
-  await lightSd.cleanAllPlatforms();
-  await lightSd.buildAllPlatforms();
-
-  const darkSd = new StyleDictionary(darkConfig);
-  await darkSd.cleanAllPlatforms();
-  await darkSd.buildAllPlatforms();
+    const styleDictionary = new StyleDictionary(config);
+    await styleDictionary.cleanAllPlatforms();
+    await styleDictionary.buildAllPlatforms();
+  }
 }
 
 const relevantTsCategories = new Set(['time']);
 const tsConfig: Config = {
-  source: [`tokens/default/**/!(*.${colorSchemes.join(`|*.`)}).json5`],
+  source: [`tokens/default/**/${colorSchemeExclusionRegex}.json5`],
   platforms: {
     ts: {
       transforms: ['name/constant'],
@@ -104,16 +84,18 @@ const tsConfig: Config = {
         {
           destination: 'index.js',
           format: 'javascript/es6',
-          filter: (token) => relevantTsCategories.has(token.attributes.category),
+          filter: (token) => token.type && relevantTsCategories.has(token.type),
         },
         {
           destination: 'index.d.ts',
           format: 'typescript/es6-declarations',
-          filter: (token) => relevantTsCategories.has(token.attributes.category),
+          filter: (token) => token.type && relevantTsCategories.has(token.type),
         },
       ],
     },
   },
 };
 
-await new StyleDictionary(tsConfig).buildAllPlatforms();
+const styleDictionary = new StyleDictionary(tsConfig);
+await styleDictionary.cleanAllPlatforms();
+await styleDictionary.buildAllPlatforms();

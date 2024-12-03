f37f598fae6a46361ab78e28acef2ab170f8b7ea
QuietNatu <quietnatu@outlook.com>
2024-12-03 12:21:32 +0000

    chore: reduce testing library log noise when test fails

--- packages/ui-angular/src/test/setup-tests.ts
+++ packages/ui-angular/src/test/setup-tests.ts
@@ -11,6 +11,7 @@ import { GlobaltTestingSetupModule } from './global-testing-setup.module';
 import { toHaveNoViolations } from 'jasmine-axe';
 import { deleteAllCookies } from '@natu/utils';
 import { mockI18n } from '../mocks/i18n';
+import { configure } from '@testing-library/angular';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -22,6 +23,20 @@ beforeAll(async () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   jasmine.addMatchers(JasmineDOM);
   jasmine.addMatchers(toHaveNoViolations);
+
+  configure({
+    dom: {
+      // prevents large unhelpful HTML DOM outputs on failed tests
+      getElementError: (message) => {
+        const error = new Error(message ?? undefined);
+        // eslint-disable-next-line functional/immutable-data
+        error.name = 'TestingLibraryElementError';
+        // eslint-disable-next-line functional/immutable-data
+        error.stack = undefined;
+        return error;
+      },
+    },
+  });
 
   await mockI18n();
 });

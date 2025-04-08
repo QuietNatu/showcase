dd24d7a0c7aa5f5f98e78ff56b6746e4582553e1
QuietNatu <quietnatu@outlook.com>
2025-04-08 23:08:46 +0100

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};

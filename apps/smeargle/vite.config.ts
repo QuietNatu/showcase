1dbc693735405ecb44408cf9bc034df7c391d6bc
QuietNatu <quietnatu@outlook.com>
2025-04-11 00:14:22 +0100

    feat: configure browserslist for apps

--- apps/smeargle/vite.config.ts
+++ apps/smeargle/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+
+    build: {
+      target: browserslistToEsbuild(),
+    },
 
     server: {
       open: mode !== 'test',

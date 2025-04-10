798f628ab30a22fd0076d11d852ec284f2ccd1d7
QuietNatu <quietnatu@outlook.com>
2025-04-10 01:04:57 +0100

    feat: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,24 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-css-modules'],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-plugin-logical-css',
+    'stylelint-no-unsupported-browser-features',
+  ],
+  ignoreFiles: ['./node_modules/**', '**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+  rules: {
+    'alpha-value-notation': 'number',
+    'custom-property-empty-line-before': [
+      'always',
+      { except: ['first-nested', 'after-comment', 'after-custom-property'] },
+    ],
+    'order/properties-alphabetical-order': true,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'plugin/no-unsupported-browser-features': true,
+    'plugin/use-logical-properties-and-values': true,
+    'plugin/use-logical-units': true,
+  },
+};

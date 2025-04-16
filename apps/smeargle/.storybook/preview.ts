29e1c44e72258d33e60656098ec077f876086d04
QuietNatu <quietnatu@outlook.com>
2025-04-16 10:44:45 +0100

    feat(smeargle): add storybook

---
+++ apps/smeargle/.storybook/preview.ts
@@ -0,0 +1,24 @@
+import '../src/styles/styles.scss';
+
+import type { Preview } from '@storybook/react';
+import { A11yParameters } from '@storybook/addon-a11y';
+
+const preview: Preview = {
+  parameters: {
+    // TODO: configure axe
+    a11y: {} satisfies A11yParameters['a11y'],
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+      },
+    },
+  },
+};
+
+export default preview;
--- apps/smeargle/src/test/setup-tests.ts
+++ apps/smeargle/src/test/setup-tests.ts
@@ -1,1 +1,9 @@
 // Setup tests here
+
+import { beforeAll } from 'vitest';
+import { setProjectAnnotations } from '@storybook/react';
+import * as projectAnnotations from '../../.storybook/preview';
+
+const project = setProjectAnnotations([projectAnnotations]);
+
+beforeAll(project.beforeAll);

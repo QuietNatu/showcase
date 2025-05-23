6e13a17939bb45da0be9f37c0a2db600f3a4a609
QuietNatu <quietnatu@outlook.com>
2025-05-23 13:01:47 +0100

    feat(styles): add font-face

--- packages/styles/build-tokens.ts
+++ packages/styles/build-tokens.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import StyleDictionary, { Config } from 'style-dictionary';
-import { formats, transforms, actions } from 'style-dictionary/enums';
+import { formats, transforms } from 'style-dictionary/enums';
 import pc from 'picocolors';
 
 const themes = ['rotom', 'rotom-dark', 'smeargle', 'smeargle-dark'];
@@ -30,14 +30,13 @@ function createThemeConfig(theme: string): Config {
   };
 }
 
-async function buildTokens(config: Config) {
+async function buildStyleSheets(config: Config) {
   const styleDictionary = new StyleDictionary(config);
 
-  await styleDictionary.cleanAllPlatforms();
   await styleDictionary.buildAllPlatforms();
 }
 
-async function buildAllTokens() {
+async function buildAllStyleSheets() {
   console.log('Build started...');
 
   // eslint-disable-next-line functional/no-loop-statements
@@ -45,75 +44,11 @@ async function buildAllTokens() {
     console.log('\n==============================================');
     console.log(`\nProcessing: [${theme}]`);
 
-    await buildTokens(createThemeConfig(theme));
+    await buildStyleSheets(createThemeConfig(theme));
   }
-
-  // TODO: custom format,
-  await buildTokens({
-    source: ['tokens/globals/asset.json'],
-    platforms: {
-      'font-face': {
-        buildPath: 'dist/tokens/',
-        files: [
-          {
-            destination: 'fonts.css',
-            format: ({ dictionary: { allTokens }, options }) => {
-              const fontPathPrefix = options.fontPathPrefix || '../';
-
-              // https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/src
-              const formatsMap = {
-                woff2: 'woff2',
-                woff: 'woff',
-                ttf: 'truetype',
-                otf: 'opentype',
-                svg: 'svg',
-                eot: 'embedded-opentype',
-              };
-
-              return allTokens
-                .reduce((fontList, prop) => {
-                  const {
-                    attributes: { family, weight, style },
-                    formats,
-                    value: path,
-                  } = prop;
-
-                  const urls = formats.map(
-                    (extension) =>
-                      `url("${fontPathPrefix}${path}.${extension}") format("${formatsMap[extension]}")`,
-                  );
-
-                  const fontCss = [
-                    '@font-face {',
-                    `\n\tfont-family: "${family}";`,
-                    `\n\tfont-style: ${style};`,
-                    `\n\tfont-weight: ${weight};`,
-                    `\n\tsrc: ${urls.join(',\n\t\t\t ')};`,
-                    '\n\tfont-display: fallback;',
-                    '\n}\n',
-                  ].join('');
-
-                  fontList.push(fontCss);
-
-                  return fontList;
-                }, [])
-                .join('\n');
-            },
-            options: {
-              fontPathPrefix: '../',
-              formatting: {
-                fileHeaderTimestamp: true,
-              },
-            },
-          },
-        ],
-        actions: [actions.copyAssets], // TODO: custom copy instead of this?
-      },
-    },
-  });
 
   console.log('\n==============================================');
   console.log(pc.green('\nBuild completed!'));
 }
 
-void buildAllTokens();
+void buildAllStyleSheets();

eaaffcadf9db0ee52e287ccbe5c78e1f6756750f
QuietNatu <quietnatu@outlook.com>
2025-12-12 02:46:23 +0000

    chore: bump storybook to v10

---
+++ packages/styles/vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+
+    test: {
+      globals: true,
+      css: true,
+      restoreMocks: true,
+      unstubEnvs: true,
+      unstubGlobals: true,
+      include: ['src/test/**/*.test.{js,jsx,ts,tsx}'],
+      setupFiles: [],
+      environment: 'happy-dom',
+      reporters: ['default'],
+    },
+  };
+});
--- packages/ui-react/.storybook/main.ts
+++ packages/ui-react/.storybook/main.ts
@@ -1,13 +1,9 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import type { StorybookConfig } from '@storybook/react-vite';
 
-import { join, dirname } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
 function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, 'package.json')));
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
 
 const config: StorybookConfig = {

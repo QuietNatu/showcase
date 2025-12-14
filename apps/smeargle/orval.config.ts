599f8ebdd0780bfbb89960b7946ccca7a7a79010
QuietNatu <quietnatu@outlook.com>
2025-12-14 22:53:32 +0000

    feat(smeargle): configure openapi client generation

---
+++ apps/smeargle/orval.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  api: {
+    input: '../api/openapi.yaml',
+    output: {
+      clean: true,
+      client: 'axios-functions',
+      target: './src/gen/api/endpoints',
+      schemas: './src/gen/api/models',
+      mode: 'tags-split',
+      indexFiles: false,
+      mock: {
+        type: 'msw',
+        delay: false,
+        useExamples: false,
+      },
+      override: {
+        components: {
+          schemas: {
+            suffix: 'Dto',
+          },
+          responses: {
+            suffix: 'Response',
+          },
+          parameters: {
+            suffix: 'Params',
+          },
+          requestBodies: {
+            suffix: 'Bodies',
+          },
+        },
+      },
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+  apiZod: {
+    input: '../api/openapi.yaml',
+    output: {
+      client: 'zod',
+      fileExtension: '.zod.ts',
+      target: './src/gen/api/endpoints',
+      mode: 'tags-split',
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+});

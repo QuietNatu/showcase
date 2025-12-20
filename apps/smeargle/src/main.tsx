9c198a371413184634e693e1dff0a398a4a44f4d
QuietNatu <quietnatu@outlook.com>
2025-12-20 02:54:53 +0000

    feat(smeargle): setup server mocking

--- apps/smeargle/src/main.tsx
+++ apps/smeargle/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './styles/styles.scss';
 import { App } from './app/app.tsx';
 
+if (import.meta.env.VITE_ENABLE_MOCKING === 'true') {
+  const { startWorker } = await import('./mocks/api/browser-development.ts');
+  await startWorker();
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- it will always exist
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

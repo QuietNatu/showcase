cada90b3ea733e2e83c6da62ff443299cf111e79
QuietNatu <quietnatu@outlook.com>
2025-12-25 02:37:30 +0000

    test(smeargle): add router tests

---
+++ apps/smeargle/src/app/routes/products/index.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
+import { MockRouter } from '../../../mocks/router';
+import { page } from 'vitest/browser';
+
+test('renders', async () => {
+  await render(<MockRouter initialEntry="/products" />);
+
+  await expect.element(page.getByRole('heading', { name: 'Products' })).toBeInTheDocument();
+});

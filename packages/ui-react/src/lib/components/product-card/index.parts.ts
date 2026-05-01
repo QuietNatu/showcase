da776183487596f8513e375a00e28910c9b115ba
QuietNatu <quietnatu@outlook.com>
2026-05-01 23:27:20 +0100

    chore: cleanup

--- packages/ui-react/src/lib/components/product-card/index.parts.ts
+++
@@ -1,6 +0,0 @@
-export {
-  NatuProductCardHeading as Heading,
-  NatuProductCardInteractable as Interactable,
-  NatuProductCardLink as Link,
-  NatuProductCardRoot as Root,
-} from './product-card';
--- packages/ui-react/src/lib/components/product-card/index.ts
+++
@@ -1,1 +0,0 @@
-export * as NatuProductCard from './index.parts';

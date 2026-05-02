7335ead258acf29e7299548293698509d6424d80
QuietNatu <quietnatu@outlook.com>
2026-05-02 13:58:16 +0100

    chore: exclude tanstack routes from react-refresh eslint plugin

--- apps/smeargle/eslint.config.js
+++ apps/smeargle/eslint.config.js
@@ -62,6 +62,12 @@ export default defineConfig(
   ...shared.configs.playwright,
   ...shared.configs.vrt,
   ...restrictImports,
+  {
+    files: ['src/app/routes/**/*.{js,ts,jsx,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
   ...shared.configs.prettier,
   {
     languageOptions: {

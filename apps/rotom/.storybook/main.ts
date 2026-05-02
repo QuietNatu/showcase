600b0435b3ba5ca3e07b5e383a4b783d3f57a0ed
QuietNatu <quietnatu@outlook.com>
2026-05-02 22:26:07 +0100

    chore(deps): update dependency storybook to v10.3.6

--- apps/rotom/.storybook/main.ts
+++ apps/rotom/.storybook/main.ts
@@ -1,37 +1,20 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import type { StorybookConfig } from '@analogjs/storybook-angular';
-import type { StorybookConfigVite } from '@storybook/builder-vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { mergeConfig } from 'vite';
+import type { StorybookConfig } from '@storybook/angular';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): string {
+function getAbsolutePath(value: string) {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
-
-const config: StorybookConfig & StorybookConfigVite = {
+const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-a11y'), getAbsolutePath('@storybook/addon-docs')],
   framework: {
     name: getAbsolutePath('@analogjs/storybook-angular'),
     options: {},
-  },
-  viteFinal: async (config) => {
-    return mergeConfig(config, {
-      plugins: [tsconfigPaths({ configNames: ['tsconfig.json'] })],
-      define: {
-        STORYBOOK_ANGULAR_OPTIONS: JSON.stringify({
-          experimentalZoneless: true,
-        }),
-      },
-      resolve: {
-        alias: { 'zone.js': fileURLToPath(import.meta.resolve('./empty-zone.ts')) },
-      },
-    });
   },
 };
 export default config;
--- packages/ui-angular/.storybook/empty-zone.ts
+++
@@ -1,1 +0,0 @@
-/* eslint-disable-next-line unicorn/no-empty-file -- needed to stub zone.js import from storybook since it still does not have proper zoneless support */

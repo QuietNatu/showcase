67f6919dbf2de6fccfe0c4eaed34f36310d1dd3b
QuietNatu <quietnatu@outlook.com>
2026-05-03 22:04:29 +0100

    chore: migrate stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,5 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-css-modules'],
   plugins: [
     'stylelint-scss',
     'stylelint-order',
@@ -8,6 +7,11 @@ export default {
     'stylelint-plugin-logical-css',
     'stylelint-no-unsupported-browser-features',
     'stylelint-gamut',
+  ],
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-css-modules',
+    'stylelint-plugin-logical-css/configs/recommended',
   ],
   ignoreFiles: ['./node_modules/**', '**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
   rules: {
@@ -24,8 +28,6 @@ export default {
     'order/properties-alphabetical-order': true,
     'plugin/declaration-block-no-ignored-properties': true,
     'plugin/no-unsupported-browser-features': [true, { ignore: ['css-nesting', 'css-when-else'] }],
-    'plugin/use-logical-properties-and-values': true,
-    'plugin/use-logical-units': true,
     'value-keyword-case': ['lower', { ignoreProperties: ['/--natu-font-family-.+/'] }],
   },
 };

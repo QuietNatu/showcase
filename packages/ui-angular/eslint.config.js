6c0999bfc8a7d17ad99febeb4ec0001ae4c3e85e
QuietNatu <quietnatu@outlook.com>
2026-05-03 03:38:44 +0100

    refactor(angular): use zoneless testing library

--- packages/ui-angular/eslint.config.js
+++ packages/ui-angular/eslint.config.js
@@ -4,12 +4,30 @@ import { defineConfig } from 'eslint/config';
 import shared from '@natu/eslint-config-shared';
 import { globalIgnores } from 'eslint/config';
 
+// TODO: improve this
+const restrictImports = defineConfig({
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: '@testing-library/angular',
+            message: 'Use @testing-library/angular/zoneless instead',
+          },
+        ],
+      },
+    ],
+  },
+});
+
 export default defineConfig(
   globalIgnores(shared.defaultIgnores),
   ...shared.configs.angular,
   ...shared.configs.storybook,
   ...shared.configs.vitest,
   ...shared.configs.vrt,
+  ...restrictImports,
   ...shared.configs.prettier,
   {
     files: ['**/*.ts'],

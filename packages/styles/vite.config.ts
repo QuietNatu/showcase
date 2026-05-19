0ecc82e641ed77f6d521e925e4a541e531e09502
QuietNatu <quietnatu@outlook.com>
2026-05-20 00:35:05 +0100

    chore(styles): support sass pkg imports

--- packages/styles/vite.config.ts
+++ packages/styles/vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react';
+import { NodePackageImporter } from 'sass-embedded';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        importers: [new NodePackageImporter()],
+      },
+    },
+  },
 });

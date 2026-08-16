da8f9fe413b5324a7ea5deb20851c8d951d8fdf0
QuietNatu <quietnatu@outlook.com>
2026-08-16 22:06:06 +0100

    docs(smeargle): add product cart and grid docs

--- apps/smeargle/src/pages/product-listing/ui/product-card/product-listing-card.tsx
+++ apps/smeargle/src/pages/product-listing/ui/product-card/product-listing-card.tsx
@@ -1,24 +1,31 @@
 import { NatuProductCard } from '@natu/ui-react/components/product-card';
 
 import { Link } from '@tanstack/react-router';
+import type { ComponentProps } from 'react';
 
 import styles from './product-listing-card.module.scss';
 
-type Props = Readonly<{
-  imageUrl: string;
-  name: string;
-  slug: string;
-  onAddToCart: () => void;
-}>;
+type Props = ComponentProps<typeof NatuProductCard.Root> &
+  Readonly<{
+    /** Teaser image url */
+    imageUrl: string;
+    /** Display name of the product */
+    name: string;
+    /** Slug for the the product detail page link */
+    slug: string;
+    /** Callback fired when product should be added to the cart */
+    onAddToCart: () => void;
+  }>;
 
 /**
- * TODO
+ * Shows a teaser of a product and provides a link to it.
+ * Allows product to be added to the cart.
  */
 export function ProductListingCard(props: Props) {
-  const { slug, name, imageUrl, onAddToCart } = props;
+  const { slug, name, imageUrl, onAddToCart, ...otherProps } = props;
 
   return (
-    <NatuProductCard.Root>
+    <NatuProductCard.Root {...otherProps}>
       <NatuProductCard.Link
         render={<Link to="/products/$productSlug" params={{ productSlug: slug }} />}
       />
--- apps/smeargle/src/pages/product-listing/ui/product-grid/product-listing-grid.tsx
+++ apps/smeargle/src/pages/product-listing/ui/product-grid/product-listing-grid.tsx
@@ -7,7 +7,7 @@ type Props = Readonly<{
 }>;
 
 /**
- * TODO
+ * Shows a list with a summary view of each product
  */
 export function ProductListingGrid(props: Props) {
   const { products } = props;
@@ -17,6 +17,7 @@ export function ProductListingGrid(props: Props) {
       {products.map((product) => (
         <li key={product.id}>
           <ProductListingCard
+            className={styles.item}
             slug={product.slug}
             name={product.name}
             imageUrl={product.imageUrl}
